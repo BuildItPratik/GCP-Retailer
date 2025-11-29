602691bbd42d1439a435533dc50ec9fdb2dec9ba
builditpratik <pratiklinkedin2005@gmail.com>
2025-11-29 21:59:15 +0530

    Added BigQuery configs and jdbc connector

--- data/ingestion/retailerMysqlToLanding.py
+++ data/ingestion/retailerMysqlToLanding.py
@@ -12,3 +12,17 @@ GCS_BUCKET = "retailer-bkt-27112025"
 LANDING_PATH = f"gs://{GCS_BUCKET}/landing/retailer-db/"
 ARCHIVE_PATH = f"gs://{GCS_BUCKET}/landing/retailer-db/archive/"
 CONFIG_FILE_PATH = f"gs://{GCS_BUCKET}/configs/retailer_config.csv"
+
+# BigQuery Configuration
+BQ_PROJECT = "numeric-replica-471213-i0"
+BQ_AUDIT_TABLE = f"{BQ_PROJECT}.temp_dataset.audit_log"
+BQ_LOG_TABLE = f"{BQ_PROJECT}.temp_dataset.pipeline_logs"
+BQ_TEMP_PATH = f"{GCS_BUCKET}/temp/"  
+
+# MySQL Configuration
+MYSQL_CONFIG = {
+    "url": "jdbc:mysql://136.116.154.6:3306/retailerDB?useSSL=false&allowPublicKeyRetrieval=true",
+    "driver": "com.mysql.cj.jdbc.Driver",
+    "user": "myuser",
+    "password": "Apple@1234"
+}
